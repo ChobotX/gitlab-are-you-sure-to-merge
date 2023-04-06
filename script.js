f5018952bb2ffc2b90680e2a6728531cec04a42f
Ondřej Cháb <ondrej.chab@websta.de>
2023-04-06 12:54:59 +0200

    Init commit

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const button = Array.from(document.querySelectorAll('span.gl-button-text'))
+const mergeButton = Array.from(document.querySelectorAll('span.gl-button-text'))
         .find(el => el.textContent === 'Merge');
 
-if (button) {
-    button.addEventListener('click', (e) => {
+if (mergeButton) {
+    mergeButton.parentNode.style.backgroundColor = 'red';
+    mergeButton.addEventListener('click', (e) => {
         if (!confirm('Are you sure?')) {
             e.preventDefault();
         }
     });
+}
+
+const approveButton = document.querySelector('button[data-qa-selector="approve_button"]');
+
+if (approveButton) {
+    approveButton.style.color = 'white';
+    approveButton.style.backgroundColor = 'green';
 }
