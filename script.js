d6c8bb7ba06304e789420ce05ccdeca50e86a78b
Ondřej Cháb <ondrej.chab@websta.de>
2023-04-06 14:44:28 +0200

    first working commit

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-const mergeButton = Array.from(document.querySelectorAll('span.gl-button-text'))
+const maxAttempts = 30;
+let attempts = 0;
+
+const updateButtons = () => {
+    attempts++;
+    const mergeButton = Array.from(document.querySelectorAll('span.gl-button-text'))
         .find(el => el.textContent === 'Merge');
 
-if (mergeButton) {
-    mergeButton.parentNode.style.backgroundColor = 'red';
-    mergeButton.addEventListener('click', (e) => {
-        if (!confirm('Are you sure?')) {
-            e.preventDefault();
+    const approveButton = document.querySelector('button[data-qa-selector="approve_button"]');
+
+    if (approveButton) {
+        approveButton.style.color = 'white';
+        approveButton.style.backgroundColor = 'green';
+    }
+
+    if (mergeButton) {
+        mergeButton.parentNode.style.backgroundColor = 'red';
+        mergeButton.addEventListener('click', (e) => {
+            if (!confirm('Are you sure?')) {
+                e.preventDefault();
+            }
+        });
+    } else {
+        if (attempts < maxAttempts) {
+            setTimeout(updateButtons, 100);
         }
-    });
-}
+    }
+};
 
-const approveButton = document.querySelector('button[data-qa-selector="approve_button"]');
-
-if (approveButton) {
-    approveButton.style.color = 'white';
-    approveButton.style.backgroundColor = 'green';
-}
+updateButtons();
